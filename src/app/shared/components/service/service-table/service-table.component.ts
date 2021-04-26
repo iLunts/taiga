c3cf160b4e16a52ae51b6dd7208dcd142e8c9959
Roman Luntsevich <shipik2008@gmail.com>
2021-04-26 23:38:10 +0300

    Added dinamic reactiveForm for serviceTable

--- src/app/shared/components/service/service-table/service-table.component.ts
+++ src/app/shared/components/service/service-table/service-table.component.ts
@@ -1,24 +1,55 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-service-table',
   templateUrl: './service-table.component.html',
-  styleUrls: ['./service-table.component.less']
+  styleUrls: ['./service-table.component.less'],
 })
 export class ServiceTableComponent implements OnInit {
-  readonly items = ['Black', 'Gold', 'Silver'];
-  readonly form = new FormGroup({
-    name: new FormControl('', Validators.required),
-    date: new FormControl(null, Validators.required),
-    color: new FormControl(null, Validators.required),
-    quantity: new FormControl(),
-    sum: new FormControl(255),
-  });
+  form: FormGroup;
+  columns: ['name', 'date', 'quantity', 'price'];
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
+    this.createForm();
   }
+
+  private createForm(): void {
+    this.form = this.formBuilder.group({
+      tableRowArray: this.formBuilder.array([this.createTableRow()]),
+    });
+  }
+
+  private createTableRow(): FormGroup {
+    return this.formBuilder.group({
+      name: new FormControl(null, {
+        validators: [Validators.required],
+      }),
+      date: new FormControl(null, {
+        validators: [Validators.required],
+      }),
+      quantity: new FormControl(1, {
+        validators: [Validators.required],
+      }),
+      price: new FormControl(0, {
+        validators: [Validators.required],
+      }),
+    });
+  }
+
+  get tableRowArray(): FormArray {
+    return this.form.get('tableRowArray') as FormArray;
+  }
+
+  addNewRow(): void {
+    this.tableRowArray.push(this.createTableRow());
+  }
+
+  deleteRow(rowIndex: number): void {
+    this.tableRowArray.removeAt(rowIndex);
+  }
+
 
 }
--- src/app/shared/components/service/service.module.ts
+++ src/app/shared/components/service/service.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { ServicePanelComponent } from './service-panel/service-panel.component';
 import { TuiLetModule } from '@taiga-ui/cdk';
-import { TuiGroupModule, TuiLabelModule, TuiLoaderModule, TuiTableModeModule } from '@taiga-ui/core';
+import { TuiButtonModule, TuiGroupModule, TuiLabelModule, TuiLoaderModule, TuiTableModeModule } from '@taiga-ui/core';
 import { ServiceTableComponent } from './service-table/service-table.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { TuiInputCountModule, TuiInputDateModule, TuiInputModule, TuiInputNumberModule, TuiSelectModule } from '@taiga-ui/kit';
@@ -19,6 +19,7 @@ import { TuiTableModule } from '@taiga-ui/addon-table';
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
+    TuiButtonModule,
     TuiGroupModule,
     TuiInputCountModule,
     TuiInputDateModule,
