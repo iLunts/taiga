b5d39ac84af89970ae3d7df34e2c4afb51f4a8fb
Roman Luntsevich <shipik2008@gmail.com>
2021-11-12 01:46:26 +0300

    Removed table in contractor list page

--- src/app/pages/admin/contractor/list/list.component.ts
+++ src/app/pages/admin/contractor/list/list.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
-import { Company, Contractor } from 'src/app/models/company.model';
-import { CompanyService } from 'src/app/services/company.service';
+
+import { Company } from 'src/app/models/company.model';
 import { ContractorService } from 'src/app/services/contractor.service';
 import { environment } from 'src/environments/environment';
 
@@ -13,24 +12,11 @@ import { environment } from 'src/environments/environment';
 export class ContractorListComponent implements OnInit {
   readonly columns = ['unp', 'name', 'action'];
   routing = environment.routing;
-  contractors$: Observable<Contractor[]>;
   isOpenAsideContractorView: boolean;
 
   constructor(private contractorService: ContractorService) {}
 
-  ngOnInit(): void {
-    this.fetch();
-  }
-
-  fetch(): void {
-    this.contractors$ = this.contractorService.getAll$();
-  }
-
-  delete(item: Contractor): void {
-    if (item) {
-      this.contractorService.delete$(item._id);
-    }
-  }
+  ngOnInit(): void {}
 
   toggleAsideContractorView(contractor: Company): void {
     this.isOpenAsideContractorView = !this.isOpenAsideContractorView;
