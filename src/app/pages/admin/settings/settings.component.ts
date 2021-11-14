6ed8055ecf00de729b53aa200e4c588d8b7ce13c
Roman Luntsevich <shipik2008@gmail.com>
2021-11-15 02:23:09 +0300

    Added Auth Layout and added aside menu for settings page

--- src/app/pages/admin/settings/settings.component.ts
+++ src/app/pages/admin/settings/settings.component.ts
@@ -8,6 +8,28 @@ import { environment } from 'src/environments/environment';
 })
 export class SettingsComponent implements OnInit {
   routing = environment.routing;
+  menuList = [
+    {
+      label: 'Компании',
+      url: this.routing.admin.settings.company,
+      icon: ''
+    },
+    {
+      label: 'Сотрудники',
+      url: this.routing.admin.settings.company,
+      icon: ''
+    },
+    {
+      label: 'Мобильные приложения',
+      url: this.routing.admin.settings.company,
+      icon: ''
+    },
+    {
+      label: 'Шаблоны',
+      url: this.routing.admin.settings.company,
+      icon: ''
+    }
+  ];
 
   constructor() {}
 
