7e0f41adf17183ccdddcb3a37ef648628e1c0480
Roman Luntsevich <shipik2008@gmail.com>
2022-10-21 23:43:26 +0300

    Added animation for invoice-notification-card

--- src/app/pages/default/home/home.component.ts
+++ src/app/pages/default/home/home.component.ts
@@ -1,17 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.less']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+  timerSubscription: Subscription;
+
   invoiceNotification = [
     {
       avatar: './../../../../assets/png/faces/1.png',
       title: 'Invoice created & sent',
       text: 'Monday at 9:15am',
-      isActive: false
+      isActive: true
     },
     {
       avatar: './../../../../assets/png/faces/2.png',
@@ -23,7 +26,7 @@ export class HomeComponent implements OnInit {
       avatar: './../../../../assets/png/faces/3.png',
       title: 'Converted to act',
       text: 'Yesterday at 10:20pm',
-      isActive: true
+      isActive: false
     },
     {
       avatar: './../../../../assets/png/faces/4.png',
@@ -33,7 +36,31 @@ export class HomeComponent implements OnInit {
     }
   ];
 
-  constructor() {}
+  constructor() {
+    let timerIndex = 0;
+
+    this.timerSubscription = interval(5000).subscribe((time) => {
+      timerIndex++;
+
+      if (timerIndex == this.invoiceNotification.length) {
+        timerIndex = 0;
+      }
+
+      this.setActiveNotification(timerIndex);
+    });
+  }
 
   ngOnInit(): void {}
+
+  ngOnDestroy(): void {
+    this.timerSubscription.unsubscribe();
+  }
+
+  setActiveNotification(index: number): void {
+    this.invoiceNotification.forEach((element) => {
+      element.isActive = false;
+    });
+
+    this.invoiceNotification[index].isActive = true;
+  }
 }
