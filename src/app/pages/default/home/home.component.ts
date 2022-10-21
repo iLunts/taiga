3281d3c644b6ebc3c69fa3ace735832c12d8d9dd
Roman Luntsevich <shipik2008@gmail.com>
2022-10-21 23:10:34 +0300

    Fixed issue with top-menu component

--- src/app/pages/default/home/home.component.ts
+++ src/app/pages/default/home/home.component.ts
@@ -9,19 +9,19 @@ export class HomeComponent implements OnInit {
   invoiceNotification = [
     {
       avatar: './../../../../assets/png/faces/1.png',
-      title: 'Estimate created & sent',
+      title: 'Invoice created & sent',
       text: 'Monday at 9:15am',
       isActive: false
     },
     {
       avatar: './../../../../assets/png/faces/2.png',
-      title: 'Estimate accepted by client',
+      title: 'Invoice accepted',
       text: 'Monday at 4:55pm',
       isActive: false
     },
     {
       avatar: './../../../../assets/png/faces/3.png',
-      title: 'Converted to invoice',
+      title: 'Converted to act',
       text: 'Yesterday at 10:20pm',
       isActive: true
     },
@@ -31,16 +31,6 @@ export class HomeComponent implements OnInit {
       text: 'Yesterday at 10:22pm',
       isActive: false
     }
-    // {
-    //   avatar: '',
-    //   title: 'Partial payment made ',
-    //   text: 'Yesterday at 2:30pm'
-    // }
-    // {
-    //   avatar: '',
-    //   title: 'Past-due reminder sent',
-    //   text: 'Today at 11:41am'
-    // }
   ];
 
   constructor() {}
